8e3e4680bfda342e02fbc487be9c71b5668d850e
ju1002 <hihill323105@gamil.com>
2025-11-19 17:39:19 +0900

    거리계산 및 위치

--- back/src/main/java/com/kh/pcar/back/station/controller/StationController.java
+++ back/src/main/java/com/kh/pcar/back/station/controller/StationController.java
@@ -1,12 +1,14 @@
 package com.kh.pcar.back.station.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.pcar.back.station.model.dto.MyStationDTO;
 import com.kh.pcar.back.station.model.service.ServiceStation;
-import com.kh.pcar.back.station.model.service.ServiceStationImpl;
 
 //import com.kh.pcar.back.station.model.service.ServiceStation;
 
@@ -14,19 +16,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/station")
 @RequiredArgsConstructor
 public class StationController {	
 	private final ServiceStation service;
+	//서비스로 사용자의 위치 정보 전송 그 후 걸러낸걸 MyStationDTO에 담음 
 	@GetMapping("/EvCharge")
-	public String station(@RequestParam(name= "lat")String lat,@RequestParam(name="lng")String lng) {
+	public List<MyStationDTO> stations(@RequestParam(name= "lat")String lat,@RequestParam(name="lng")String lng) {
 //		log.info("{}",page);
 //		log.info("{}",serviceKey);
 //		log.info("{}",perPage);
+		//현재 위치
 		log.info("{},{}",lat,lng);
-		String response = service.station(lat,lng);
-		return"";
+		List<MyStationDTO> stations = service.stations(lat,lng);
+		log.info("{}",stations);
+		return stations;
 	}
 
 }
--- back/src/main/java/com/kh/pcar/back/station/model/service/ServiceStation.java
+++ back/src/main/java/com/kh/pcar/back/station/model/service/ServiceStation.java
@@ -1,6 +1,10 @@
 package com.kh.pcar.back.station.model.service;
 
+import java.util.List;
+
+import com.kh.pcar.back.station.model.dto.MyStationDTO;
 
 public interface ServiceStation {
-	String station(String lat,String lng);
+
+	List<MyStationDTO> stations(String lat, String lng);
 }
