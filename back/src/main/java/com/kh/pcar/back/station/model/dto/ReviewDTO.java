6e9bc0bad70b5e4e9c516585406958cf4d4fc503
ju1002 <hihill323105@gamil.com>
2025-12-05 10:20:32 +0900

    commit

--- back/src/main/java/com/kh/pcar/back/station/model/dto/ReviewDTO.java
+++ back/src/main/java/com/kh/pcar/back/station/model/dto/ReviewDTO.java
@@ -5,6 +5,8 @@ package com.kh.pcar.back.station.model.dto;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,12 +19,19 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class ReviewDTO {
+	
 	private Long reviewId;
+	@NotBlank(message="아이디 값은 비어있을 수 없습니다.")
 	private String commentContent;
+	@NotBlank(message="내용 부분은 비어있을 수 없습니다.")
+	@Size(min=10,max=50, message="내용은 10글자 이상 50글자 이하만 사용할 수 있습니다.")
 	private String recommend;
 	private String stationId;
+	@NotBlank(message="충전소 아이디 값은 비어있을 수 없습니다.")
 	private Long userNo;
+	
 	private Date createdAt;
+	
 	private String notRecommend;
 	
 
