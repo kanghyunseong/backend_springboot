b02e9095ef5e46540064a99e541cc12f29249e8c
이름 <>
2025-12-09 10:46:20 +0900

    fix123

--- back/src/main/java/com/kh/pcar/back/main/model/dao/MainMapper.java
+++ back/src/main/java/com/kh/pcar/back/main/model/dao/MainMapper.java
@@ -19,6 +19,9 @@ public interface MainMapper {
 	@Select("SELECT COUNT(*) FROM TB_MEMBER")
 	String countMember();
 
+	@Select("SELECT COUNT(*) FROM TB_CAR C LEFT JOIN TB_RESERVATION R ON C.CAR_ID = R.CAR_ID WHERE R.RESERVATION_STATUS = 'Y'")
+	String countRentalCars();
+	
 	@Select("""
 			SELECT
 				   c.CAR_ID carId,
@@ -32,7 +35,7 @@ public interface MainMapper {
 			    ON
 			       c.CAR_ID = r.CAR_ID
 			 WHERE
-			       r.RESERVATION_STATUS = 'Y'
+			       r.RESERVATION_STATUS = 'N'
 			 GROUP BY
 			       c.CAR_ID, c.CAR_NAME, c.CAR_CONTENT, c.CAR_IMAGE
 			 ORDER BY
